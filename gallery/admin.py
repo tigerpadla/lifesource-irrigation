46d2a2f8dd0c1dd53a3c6f84c9b16bd2376fdaa6
tigerpadla <xythryll@gmail.com>
2026-01-26 20:10:33 +0000

    Add multi-image gallery support with carousel modal and new images

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from .models import Project, ProjectCategory
+from django.utils.html import format_html
+from .models import Project, ProjectCategory, ProjectImage
+
+
+class ProjectImageInline(admin.TabularInline):
+    """Inline admin for adding multiple images to a project"""
+    model = ProjectImage
+    extra = 3  # Show 3 empty slots by default
+    fields = ['image', 'caption', 'order', 'image_preview']
+    readonly_fields = ['image_preview']
+    ordering = ['order', 'id']
+    
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-height: 80px; max-width: 120px; object-fit: cover;" />', obj.image.url)
+        return "-"
+    image_preview.short_description = "Preview"
 
 
 @admin.register(ProjectCategory)
@@ -11,12 +27,35 @@ class ProjectCategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ['title', 'get_categories', 'location', 'featured', 'created_at']
+    list_display = ['title', 'image_preview', 'get_categories', 'location', 'image_count', 'featured', 'created_at']
     list_filter = ['categories', 'featured']
     list_editable = ['featured']
     search_fields = ['title', 'description', 'location']
-    filter_horizontal = ['categories']  # Nice dual-list widget for selecting multiple categories
+    filter_horizontal = ['categories']
+    inlines = [ProjectImageInline]
+    
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'image', 'description')
+        }),
+        ('Details', {
+            'fields': ('categories', 'location', 'featured')
+        }),
+    )
+    
+    def image_preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" style="max-height: 50px; max-width: 80px; object-fit: cover; border-radius: 4px;" />', obj.image.url)
+        return "-"
+    image_preview.short_description = "Cover"
     
     def get_categories(self, obj):
         return ", ".join([c.name for c in obj.categories.all()])
     get_categories.short_description = 'Categories'
+    
+    def image_count(self, obj):
+        count = obj.image_count
+        if count > 1:
+            return format_html('<span style="color: green; font-weight: bold;">{} images</span>', count)
+        return "1 image"
+    image_count.short_description = 'Images'
--- gallery/models.py
+++ gallery/models.py
@@ -19,7 +19,7 @@ class Project(models.Model):
     """Gallery project/photo"""
     title = models.CharField(max_length=200)
     categories = models.ManyToManyField(ProjectCategory, blank=True, related_name='projects')
-    image = models.ImageField(upload_to='gallery/')
+    image = models.ImageField(upload_to='gallery/', help_text="Main/cover image for this project")
     description = models.TextField(blank=True)
     location = models.CharField(max_length=100, blank=True, help_text="e.g., Upper West Side")
     featured = models.BooleanField(default=False, help_text="Show on homepage")
@@ -30,3 +30,31 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_all_images(self):
+        """Return all images for this project (main + additional)"""
+        images = [{'url': self.image.url, 'is_main': True}]
+        for img in self.additional_images.all():
+            images.append({'url': img.image.url, 'is_main': False, 'caption': img.caption})
+        return images
+    
+    @property
+    def image_count(self):
+        """Total number of images (main + additional)"""
+        return 1 + self.additional_images.count()
+
+
+class ProjectImage(models.Model):
+    """Additional images for a gallery project"""
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='additional_images')
+    image = models.ImageField(upload_to='gallery/')
+    caption = models.CharField(max_length=200, blank=True, help_text="Optional caption for this image")
+    order = models.PositiveIntegerField(default=0, help_text="Order of display (lower = first)")
+
+    class Meta:
+        ordering = ['order', 'id']
+        verbose_name = "Additional Image"
+        verbose_name_plural = "Additional Images"
+
+    def __str__(self):
+        return f"{self.project.title} - Image {self.order + 1}"
